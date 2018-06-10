38c72f8a204ec1146dcc6829368e162f40af4d3b
yangpan <caxc5201>
2018-06-10 20:41:18 +0800

    modify translate tool

--- crawler/translate_main.py
+++ crawler/translate_main.py
@@ -8,6 +8,7 @@ from ts_log import logger
 if __name__ == "__main__":
     # run translator every 30 minutes, max articles shall 
     # not exceed 6 in a bulk. Otherwise, memory overflow
-    logger.info('translate articles ...')
+    logger.info("translate articles ...")
+    print("tr")
     translate_articles(bulk_size=6)
     sys.exit(0)
